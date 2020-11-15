cd38edceda55f7a10b886e805fb1d07959d49be1
Lucho <l.contreras534@pascualbravo.edu.co>
2020-11-15 00:38:22 -0500

    form values required

--- src/utils/isPersonalDataValid.js
+++ src/utils/isPersonalDataValid.js
@@ -20,15 +20,23 @@ const isPersonalDataValid = ({
   if (!telefono) {
     errors.telefono = "Required"
   }
-  // console.log(String(telefono).length)
-  // if (String(telefono).length !== 7 || String(telefono).length !== 10) {
-  //   errors.telefono = "telefono incorrecto"
-  // }
   if (!correo) {
     errors.correo = "Required"
   }
   if (!date) {
     errors.date = "Required"
+  } else if (date.substr(0, 4) >= 2017) {
+    errors.date = "incorrect date"
+  }
+
+  //exceptions
+  if (String(telefono).length !== 7 && String(telefono).length !== 10) {
+    errors.telefono = "incorrect phone"
+  }
+
+  const regex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i
+  if (!regex.test(correo)) {
+    errors.correo = "incorrect email"
   }
 
   return errors
