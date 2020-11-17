0b4b04ce900a1d11fb685a5da9f7ab1be7c1eb2a
Lucho <l.contreras534@pascualbravo.edu.co>
2020-11-17 00:50:19 -0500

    view Questions and component radioGroup

---
+++ src/components/RadioGroup.tsx
@@ -0,0 +1,20 @@
+import React from "react"
+import { Box, Stack, Radio, RadioGroup, Flex } from "@chakra-ui/react"
+
+const WrapperButton = ({ question = "" }) => (
+  <RadioGroup name="pregunta">
+    <Stack spacing={5} direction="row">
+      <Flex flexDir="column" w="100%">
+        <Box m={3}>{question}</Box>
+      </Flex>
+      <Radio colorScheme="green" value="1">
+        SI
+      </Radio>
+      <Radio colorScheme="red" value="2">
+        NO
+      </Radio>
+    </Stack>
+  </RadioGroup>
+)
+
+export default WrapperButton
