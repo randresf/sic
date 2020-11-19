32d05f9c3a659b457ca3bf0fae0c22a9d020481d
Lucho <l.contreras534@pascualbravo.edu.co>
2020-11-18 23:23:34 -0500

    radio btns values

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -35,3 +35,8 @@ export const AVISO_PROTECCION_DATOS =
   "Nos tomamos muy enserio los datos personales que nos facilitas al usar nuestros servicios y por esta razon, " +
   "nos comprometemos a maneterlos completamente a salvo, a ser transparentes sobre los datos que recopilamos " +
   "y a garantizar los derechos que te asisten para controlar dicha informacion"
+
+export const TITULO_AVISO_MODAL = "AVISO"
+
+export const MENSAJE_NO_INGRESO =
+  "Si una de las opciones anteriores fue dada como un si, no permite continuar"
--- src/container/questions.tsx
+++ src/container/questions.tsx
@@ -1,29 +1,50 @@
 import React from "react"
 import Wrapper from "../components/Wrapper"
-import { Box, Flex, Heading } from "@chakra-ui/react"
+import { Box, Flex, Heading, useDisclosure } from "@chakra-ui/react"
 import PrimaryButton from "../components/PrimaryButton"
 import YesNoRadioGroup from "../components/YesNoRadioGroup"
+import ModalError from "../components/ModalError"
 import { Formik, Form } from "formik"
 import { QUESTIONS } from "../constants/index"
 import { AVISO_PROTECCION_DATOS } from "../constants/index"
+import { TITULO_AVISO_MODAL } from "../constants/index"
+import { MENSAJE_NO_INGRESO } from "../constants/index"
+
+const QuestionsMutation = `
+  mutation {
+    registrerQuestion(
+      data: 
+    ){
+      questionId
+    }
+  }
+`
 
 const Question = () => {
   const [value, setValue] = React.useState({})
   const [error, setError] = React.useState(false)
+  const { isOpen, onOpen, onClose } = useDisclosure()
 
   const onRadioGroupChange = function (questionId: number) {
     return function updateQuestionValue(radioValue: string) {
-      validate()
       setValue({ ...value, [questionId]: radioValue })
     }
   }
 
   const validate = () => {
-    const q = Object.entries(value).find(([key, value]) =>
-      value === "1" ? key : null
-    )
-    if (q && q[0]) {
+    console.log(value)
+    const arrValue = Object.values(value)
+    if (arrValue.length < 7) {
       setError(true)
+    } else {
+      setError(false)
+    }
+    const q = arrValue.find((value) => value === "1")
+    console.log(q)
+    if (q) {
+      setError(true)
+    } else {
+      setError(false)
     }
   }
 
@@ -33,11 +54,17 @@ const Question = () => {
         <Heading>Formulario salud</Heading>
         <Formik
           initialValues={{
-            id: "",
-            pregunta: "",
-            respuesta: "",
+            citizenId: "",
+            questionId: "",
+            questionText: "",
+            answer: "",
           }}
-          onSubmit={() => {}}
+          validate={(values) => {
+            console.log(values)
+          }}
+          onSubmit={(values) => {
+            console.log(values)
+          }}
         >
           {({ isSubmitting }) => (
             <Form style={{ width: "100%" }}>
@@ -50,19 +77,19 @@ const Question = () => {
                     QUESTIONS.map((prg) => (
                       <YesNoRadioGroup
                         key={prg.id}
+                        name="pregunta"
                         text={prg.question}
                         onChange={onRadioGroupChange(prg.id)}
-                        name="pregunta"
                       />
                     ))}
                   <Box mt={3}>
-                    <PrimaryButton
-                      type="submit"
-                      isDisabled={error}
-                      isLoading={isSubmitting}
-                    >
-                      Continuar
-                    </PrimaryButton>
+                    {error ? (
+                      <PrimaryButton type="submit" onClick={onOpen}>
+                        modal
+                      </PrimaryButton>
+                    ) : (
+                      <PrimaryButton type="submit">Continuar</PrimaryButton>
+                    )}
                   </Box>
                 </Flex>
               </Box>
@@ -70,6 +97,12 @@ const Question = () => {
           )}
         </Formik>
       </Flex>
+      <ModalError
+        titulo={TITULO_AVISO_MODAL}
+        contenido={MENSAJE_NO_INGRESO}
+        isOpen={isOpen}
+        onClose={onClose}
+      ></ModalError>
     </Wrapper>
   )
 }
