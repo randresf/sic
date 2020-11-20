f42abb6698b0d272ee0718b4431acea1ba729872
LuisContreras <l.contreras534@pascualbravo.edu.co>
2020-11-19 21:19:40 -0500

    avances

--- src/container/questions.tsx
+++ src/container/questions.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 import Wrapper from "../components/Wrapper"
-import { Box, Flex, Heading, useDisclosure } from "@chakra-ui/react"
+import { Box, Flex, Heading } from "@chakra-ui/react"
 import PrimaryButton from "../components/PrimaryButton"
 import YesNoRadioGroup from "../components/YesNoRadioGroup"
 import ModalError from "../components/ModalError"
@@ -10,82 +10,57 @@ import { AVISO_PROTECCION_DATOS } from "../constants/index"
 import { TITULO_AVISO_MODAL } from "../constants/index"
 import { MENSAJE_NO_INGRESO } from "../constants/index"
 
-const QuestionsMutation = `
-  mutation {
-    registrerQuestion(
-      data: 
-    ){
-      questionId
-    }
-  }
-`
-
 const Question = () => {
-  const [value, setValue] = React.useState({})
-  const [error, setError] = React.useState(false)
-  const { isOpen, onOpen, onClose } = useDisclosure()
+  const [error, setError] = React.useState(true)
 
-  const onRadioGroupChange = function (questionId: number) {
-    return function updateQuestionValue(radioValue: string) {
-      setValue({ ...value, [questionId]: radioValue })
-    }
-  }
-
-  const validate = () => {
-    console.log(value)
-    const arrValue = Object.values(value)
-    if (arrValue.length < 7) {
+  const validate = (values: any) => {
+    const arrValue = Object.values(values)
+    const q = arrValue.find(value => value === "1")
+    if (QUESTIONS.length !== arrValue.length || q) {
       setError(true)
-    } else {
-      setError(false)
+      return false
     }
-    const q = arrValue.find((value) => value === "1")
-    console.log(q)
-    if (q) {
-      setError(true)
-    } else {
-      setError(false)
-    }
+    setError(false)
+    return true
   }
 
   return (
     <Wrapper variant="small">
       <Flex alignItems="center" flex={1} p={5} flexDir="column">
         <Heading>Formulario salud</Heading>
+
+        <Heading
+          as="h5"
+          size="sm"
+          mb={6}
+          mt={6}
+          style={{ textAlign: "center" }}
+        >
+          {AVISO_PROTECCION_DATOS}
+        </Heading>
         <Formik
-          initialValues={{
-            citizenId: "",
-            questionId: "",
-            questionText: "",
-            answer: "",
+          initialValues={{}}
+          validate={values => {
+            validate(values)
           }}
-          validate={(values) => {
-            console.log(values)
-          }}
-          onSubmit={(values) => {
-            console.log(values)
-          }}
+          onSubmit={values => {}}
         >
           {({ isSubmitting }) => (
             <Form style={{ width: "100%" }}>
-              <Box m={4} style={{ textAlign: "center" }}>
-                {AVISO_PROTECCION_DATOS}
-              </Box>
               <Box>
                 <Flex flexDir="column" w="100%">
                   {QUESTIONS &&
-                    QUESTIONS.map((prg) => (
+                    QUESTIONS.map(prg => (
                       <YesNoRadioGroup
                         key={prg.id}
-                        name="pregunta"
+                        name={prg.id}
                         text={prg.question}
-                        onChange={onRadioGroupChange(prg.id)}
                       />
                     ))}
                   <Box mt={3}>
                     {error ? (
-                      <PrimaryButton type="submit" onClick={onOpen}>
-                        modal
+                      <PrimaryButton disabled type="submit">
+                        Continuar
                       </PrimaryButton>
                     ) : (
                       <PrimaryButton type="submit">Continuar</PrimaryButton>
@@ -97,12 +72,6 @@ const Question = () => {
           )}
         </Formik>
       </Flex>
-      <ModalError
-        titulo={TITULO_AVISO_MODAL}
-        contenido={MENSAJE_NO_INGRESO}
-        isOpen={isOpen}
-        onClose={onClose}
-      ></ModalError>
     </Wrapper>
   )
 }
