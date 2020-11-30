547b93e589b1278eb924762c3bf3dfa40f045831
Lucho <l.contreras534@pascualbravo.edu.co>
2020-11-30 00:09:09 -0500

    finish tkt about reservation list in formDataUser

--- src/container/reservationsList.tsx
+++ src/container/reservationsList.tsx
@@ -1,5 +1,6 @@
-import { DeleteIcon } from "@chakra-ui/icons"
+import { SearchIcon } from "@chakra-ui/icons"
 import {
+  Box,
   Center,
   Flex,
   Heading,
@@ -8,9 +9,10 @@ import {
   useToast,
   Wrap,
   WrapItem,
+  Link,
 } from "@chakra-ui/react"
 import React, { useState } from "react"
-import { Link } from "react-router-dom"
+import { useHistory } from "react-router-dom"
 // import Loading from "../components/Loading"
 import { useCancelReservationMutation } from "../generated/graphql"
 import MSGS from "../locale/es"
@@ -30,42 +32,54 @@ const ReservationsList = ({
 }: ReservationListProps) => {
   const [, cancelReserve] = useCancelReservationMutation()
   const [saving, setLoading] = useState(false)
+  const history = useHistory()
   const toast = useToast()
   if (!reservations) return null
-  const onCancel = async (reservationId: string) => {
-    setLoading(true)
-    const res = await cancelReserve({ reservationId, userId })
-    setLoading(false)
-    if (res.error)
-      return toast({
-        title: "no se pudo cancelar la reserva",
-        description: res.error.message,
-        isClosable: true,
-        duration: 3000,
-        status: "error",
-      })
-    toast({
-      title: "actualizado correctamente",
-      description: "",
-      isClosable: true,
-      duration: 3000,
-      status: "success",
-    })
-    return cb({ target: { value: userId } })
-  }
+  // const onCancel = async (reservationId: string) => {
+  //   setLoading(true)
+  //   const res = await cancelReserve({ reservationId, userId })
+  //   setLoading(false)
+  //   if (res.error)
+  //     return toast({
+  //       title: "no se pudo cancelar la reserva",
+  //       description: res.error.message,
+  //       isClosable: true,
+  //       duration: 3000,
+  //       status: "error",
+  //     })
+  //   toast({
+  //     title: "actualizado correctamente",
+  //     description: "",
+  //     isClosable: true,
+  //     duration: 3000,
+  //     status: "success",
+  //   })
+  //   return cb({ target: { value: userId } })
+  // }
   return (
     <Flex flexDir="column">
-      <Heading as="h3" size="md">
+      <Heading mt={3} mb={2} as="h3" size="md">
         {MSGS.RESERVATIONS_HEADING}
       </Heading>
       <Wrap>
         {reservations?.map((r: any) => (
-          <WrapItem key={v4()}>
-            <Center w="80%">
-              <Link to={`/reservation/${r.id}`}>
-                {r.meeting.title}
+          <WrapItem mb={3} w="70%" key={v4()}>
+            <Center>
+              <Box w="100%">
+                <Link href={`/reservation/${r.id}`}>{r.meeting.title}</Link>
                 <Text size="sm">({r.meeting.meetingDate})</Text>
-              </Link>
+              </Box>
+              <Flex>
+                <IconButton
+                  ml={5}
+                  mr={3}
+                  aria-label="Search reservation"
+                  onClick={() => {
+                    history.push(`/reservation/${r.id}`)
+                  }}
+                  icon={<SearchIcon />}
+                />
+              </Flex>
             </Center>
           </WrapItem>
         ))}
