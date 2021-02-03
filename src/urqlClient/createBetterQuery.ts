64945db0f9b00ea610f2fc197afa5e36da684079
randresf <andres.rtpo.pino@gmail.com>
2021-02-03 00:55:01 -0500

    build of superadminUI

--- src/urqlClient/createBetterQuery.ts
+++ src/urqlClient/createBetterQuery.ts
@@ -1,7 +1,7 @@
 import { Cache, QueryInput } from "@urql/exchange-graphcache"
 
 // this is used to redefine the types for typescript for
-// the cacheExhange mutation
+
 export function betterUpdateQuery<Result, Query>(
   cache: Cache,
   qi: QueryInput,
