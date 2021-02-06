28d6c9316f41e5449d5483f8386b4eb6d7bb7b14
Lucho <l.contreras534@pascualbravo.edu.co>
2021-02-06 00:33:34 -0500

    advances

--- src/container/Reservations.tsx
+++ src/container/Reservations.tsx
@@ -1,17 +1,25 @@
 import React from "react"
-import { Flex } from "@chakra-ui/react"
+import { Flex, Input, InputGroup, InputRightElement } from "@chakra-ui/react"
+import { Search2Icon } from "@chakra-ui/icons"
+import Meetings from "../pages/Dashboard/tabs/Meetings"
 
 const Reservations = () => {
   return (
     <Flex flexDir="column">
-      <Flex
-        mt={5}
-        border="1px solid #606060 "
-        flex={1}
-        alignItems="center"
-        flexWrap="wrap"
-      >
-        Prueba
+      <InputGroup w="25%">
+        <Input placeholder="Cedula" />
+        <InputRightElement children={<Search2Icon color="#3e4685" />} />
+      </InputGroup>
+      <Flex flexDir="column">
+        <Flex
+          mt={5}
+          border="1px solid #606060 "
+          flex={1}
+          alignItems="center"
+          flexWrap="wrap"
+        >
+          <Meetings />
+        </Flex>
       </Flex>
     </Flex>
   )
